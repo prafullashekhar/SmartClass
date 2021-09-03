74bf5a95f82b81d32366cd21e0de54897a690c97
prafullashekhar <prafullashekhar@gmail.com>
2021-09-03 21:12:15 +0530

    initial commit

---
+++ SmartClass/app/src/main/java/com/hackersAtHeist/smartclass/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.hackersAtHeist.smartclass.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackersAtHeist.smartclass.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
